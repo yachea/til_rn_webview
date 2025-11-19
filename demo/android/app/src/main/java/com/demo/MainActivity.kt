9bf528188cf10c1d35e1bad05722bfa232783e3b
yachea <dev.yachea@gmail.com>
2025-11-19 16:59:04 +0900

    [docs] 기본 배포 셋팅

--- demo/android/app/src/main/java/com/demo/MainActivity.kt
+++ demo/android/app/src/main/java/com/demo/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.demo
 
+// 추가
+import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+// 추가
+import org.devio.rn.splashscreen.SplashScreen
+
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +26,10 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // 추가
+  override fun onCreate(savedInstanceState: Bundle?) {
+      SplashScreen.show(this)
+      super.onCreate(savedInstanceState)
+  }
 }
